72edb56b15540e6a88a7941b852f19800151fdfa
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-09-14 17:21:56 -0700

    working parsing for data into 3dimensional numpy array for use in tensor.stack

--- model2.py
+++ model2.py
@@ -1,14 +1,57 @@
 import torch
 import pandas as pd
 import sqlite3
+import numpy as np
 
 conn = sqlite3.connect('db.db')
 data = pd.read_sql_query("SELECT * from main", conn)
+# delete all columns except for datetime, buoy_id, WVHT, MWD, and apd
+data = data[['datetime', 'buoy_id', 'WVHT', 'MWD', 'APD']]
+
+# just get hourly data, do not worry about minutes
+data['datetime'] = pd.to_datetime(data['datetime'])
+data['datetime'] = data['datetime'].dt.floor('H')
+
+# selecet data that is only after 7/31/2023
+data = data[data['datetime'] > '2023-07-31']
+
+# for each unique buoy_id, create a numbered mapping
+buoy_ids = data['buoy_id'].unique()
+map = {}
+for (k, v) in enumerate(buoy_ids): 
+    map[v] = k
+
+
+# delete rows where data['APD'] == 'MM'
+data = data[data['APD'] != 'MM']
+
+# format data to display in days and hours
 
 #convert types of WVHT, MWD, and APD to float
 data['WVHT'] = data['WVHT'].astype(float)
 data['MWD'] = data['MWD'].astype(float)
 data['APD'] = data['APD'].astype(float)
+
+# the index should be days in datetime days and hours, not every date
+ptable = data.pivot_table(index = 'datetime', columns = 'buoy_id', values = ['WVHT', 'MWD', 'APD'], aggfunc = 'mean')
+# ptable = data.pivot_table(index = 'datetime', columns = ['WVHT', 'MWD', 'APD'], values = 'buoy_id', aggfunc = 'mean')
+# print(ptable)
+
+
+# read sql data into numpy array
+ptable = ptable.values
+reshaped_array = ptable.reshape(ptable.shape[0], ptable.shape[1]) 
+# using reshaped array, split into 3 arrays, one for column 0 to 76, then 76 to 152, then 152 to 228, concatenate them so the resulting shape is (1099, 76, 3)
+reshaped_array = np.array((reshaped_array[:, :76], reshaped_array[:, 76:152], reshaped_array[:, 152:228]))
+
+nbuoys = data['buoy_id'].unique()
+# reshaped_array =[reshaped_array[:][:nbuoys], reshaped_array[:][nbuoys:nbuoys + nbuoys], reshaped_array[:][nbuoys:]]
+
+print(reshaped_array.shape)
+print(reshaped_array)
+
+
+# print(data)
 
 # create a new column that days ago from the date of the most recent buoy that is '46221'
 use_date = data[data['buoy_id'] == '46221'].iloc[0]['datetime']
@@ -19,14 +62,3 @@ data['days_ago'] = data.apply(lambda row: (pd.to_datetime(use_date) - pd.to_date
 all_buoys = data[data['buoy_id'] != '46221']
 output_buoy = data[data['buoy_id'] == '46221']
 
-# write a function to create a pytorch tensor from the data. Each row will be a vector. Get the wvht, mwd, and apd from each buoy for 1 day before the date of the most recent buoy 46221
-def create_tensor(df):
-    # get the rows that are 1 day before the most recent buoy 46221
-    df = all_buoys[all_buoys['days_ago'] == 1]
-    # get the wvht, mwd, and apd from each buoy
-    df = df[['WVHT', 'MWD', 'APD']]
-    # convert to a pytorch tensor
-    tensor = torch.tensor(df.values)
-    return tensor
-
-create_tensor(all_buoys)
