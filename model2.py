e04ff5be554f3ff113a02f466bd250d98ff67373
evancoons22 <96972938+evancoons22@users.noreply.github.com>
2023-09-21 21:14:47 -0600

    db change and model update

--- model2.py
+++ model2.py
@@ -6,13 +6,14 @@ import functions
 
 conn = sqlite3.connect('db.db')
 data = pd.read_sql_query("SELECT * from main", conn)
-data = functions.cleanData(data)
+# data = functions.cleanData(data)
 
 all_buoys = data[data['buoy_id'] != '46221']
 output_buoy = data[data['buoy_id'] == '46221']
 
 
-data = functions.buildnparray(functions.cleanData(data)))
+data = functions.buildnparray(functions.cleanData(data))
+print(data)
 
 
 # create a new column that days ago from the date of the most recent buoy that is '46221'
